dca6c149a06f659aaf9fe6320bd22ec8a029a60f
wonchul <kim.wonchul@aiv.ai>
2024-12-16 19:36:46 +0900

    initial

---
+++ scripts/train_labelme.py
@@ -0,0 +1,24 @@
+from ultralytics import YOLO, settings
+settings.update({'wandb': False})
+
+
+model = YOLO("/HDD/weights/yolov11/yolo11x.pt")
+
+data={'path': '/DeepLearning/_athena_tests/datasets/rectangle2/split_dataset_unit',
+        'train': 'train',
+        'val': 'val',
+        'names': {0: 'RING', 1: 'DUST', 2: 'SCRATCH', 3: 'FOLD', 4: 'DAMAGE', 5: 'LINE', 
+                  6: 'BOLD', 7: 'BURR', 8: 'BUBBLE', 9: 'TIP', 10: 'REACT'}
+    }
+
+train_results = model.train(
+    data=data,  
+    epochs=300, 
+    imgsz=640, 
+    device="0,1", 
+    lrf=0.001,
+    batch=4,
+    
+    label_format='labelme',
+    roi_info=[[]]
+)
--- scripts/train_yolo.py
+++ scripts/train_yolo.py
@@ -5,11 +5,12 @@ settings.update({'wandb': False})
 model = YOLO("/HDD/weights/yolov11/yolo11x.pt")
 
 train_results = model.train(
-    data="/HDD/_projects/github/custom_ultralytics/scripts/cfgs/test.yaml",  
+    data="/HDD/_projects/github/custom_ultralytics/scripts/cfgs/unit.yaml",  
     epochs=300, 
     imgsz=1024, 
-    device="0,1,2,3", 
+    device="0,1", 
     lrf=0.001,
-    batch=16,
+    batch=4,
+    optimizer="SGD"
 )
 
