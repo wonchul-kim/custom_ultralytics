7a61a08240da1b1e13f59100e6f11fa7f9389386
wonchul <kim.wonchul@aiv.ai>
2024-12-17 16:32:49 +0900

    roi is applied

--- ultralytics/data/dataset.py
+++ ultralytics/data/dataset.py
@@ -166,10 +166,10 @@ class YOLODataset(BaseDataset):
                             total=len(self.im_files),
                         )
                     )
-                    print(">>>>>>>>>>>>>> ", _roi, len(self.im_files), len(self.label_files))
-                    for _img_files, _label_files, _prefix, _class2label, __roi, _roi_from_json in zip(self.im_files, self.label_files, repeat(self.prefix), repeat(class2label), repeat(_roi), repeat(False)):
-                        args = (_img_files, _label_files, _prefix, _class2label, __roi, _roi_from_json)
-                        __labels = verify_labelme(args)
+                    # print(">>>>>>>>>>>>>> ", _roi, len(self.im_files), len(self.label_files))
+                    # for _img_files, _label_files, _prefix, _class2label, __roi, _roi_from_json in zip(self.im_files, self.label_files, repeat(self.prefix), repeat(class2label), repeat(_roi), repeat(False)):
+                    #     args = (_img_files, _label_files, _prefix, _class2label, __roi, _roi_from_json)
+                    #     __labels = verify_labelme(args)
             else:
                 desc = f"{self.prefix}Scanning '{path.parent / path.stem}' images and labels..."
                 pbar_list.append(
